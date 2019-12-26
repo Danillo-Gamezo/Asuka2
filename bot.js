602c8847aac218330eb9690d69cfe31cb9d8b616
Antoine Joly <jolyantoine@zoho.com>
2019-12-27 00:15:35 +0100

    Baka with image working but very slow

--- bot.js
+++ bot.js
@@ -1,11 +1,23 @@
 //Adding token from env variables
 require('dotenv').config()
 
-// require the discord.js module
+// require the discord.js and fs modules
 const Discord = require('discord.js');
+const fs = require('fs');
 
 // create a new Discord client
 const client = new Discord.Client();
+const prefix = ".asuka ";
+
+//Setting lists of commands
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+//Importing the commands from the folder
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
@@ -14,12 +26,36 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-	if (message.content === '.asuka baka') {
-		// send back "Pong." to the channel the message was sent in
-		message.channel.send('Anta Baka !');
-	}
+    // Our bot needs to know if it will execute a command
+	// It will listen for messages that will start with `.asuka`
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(' ');
+	const command = args.shift().toLowerCase();
+	const mentionned_user = getUserFromMention(args[0]);
+
+	switch(command) {
+		case 'baka':
+			client.commands.get('baka').execute(message, args, mentionned_user);
+		break;
+		// Just add any case commands if you want to..
+		default:
+			message.channel.send("It's not a command. What are you, stupid ?!")
+		break;
+	 }
 });
 
 // login to Discord with your app's token
 client.login(process.env.BOT_TOKEN);
 
+//Functions
+function getUserFromMention(mention) {
+	if (!mention) return;
+	if (mention.startsWith('<@') && mention.endsWith('>')) {
+		mention = mention.slice(2, -1);
+		if (mention.startsWith('!')) {
+			mention = mention.slice(1);
+		}
+		return client.users.get(mention);
+	}
+}
---
+++ commands/baka.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+
+module.exports = {
+	name: 'baka',
+	description: 'Reply with Anta Baka',
+	execute(message, args, mentionned_user) {
+		if(mentionned_user) {
+			fs.readdir('./images/Baka', (err, files) => {
+				const list_img = []
+				files.forEach(file => {
+				  list_img.push(file);
+				});
+				const img = list_img[Math.round(Math.random() * (list_img.length))];
+				const Embed = new Discord.RichEmbed()
+					.setColor('#ff0000')
+					.setDescription('Baka-'+mentionned_user+" !")
+					.attachFiles(['./images/Baka/'+img])
+					.setImage('attachment://'+img)
+				message.channel.send(Embed);
+			});
+			
+		} else {
+			message.channel.send('You have to tag someone, you stupid !');
+		}
+	},
+};
